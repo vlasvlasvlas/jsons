6706255f81bbf05d670e71befb9362eecd4a8678
Vladimiro Bellini <sonido@gmail.com>
2021-02-19 10:35:53 -0300

    Create panamacomprasdf2.py
    scrapea los resultados obtenidos en 99.csv (guarda contenidos de todos los contratos para un posterior scrapeo con bs)

---
+++ panamacomprasdf2.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import simplejson as json
+import bs4 as bs
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from time import sleep
+df = pd.read_csv('../data/panama/99.csv')
+for link in df['link']:
+    print (link)
+    url = link
+    chromeOptions = webdriver.ChromeOptions()
+    browser = webdriver.Chrome(r"C:\Python\chromedriver.exe",options=chromeOptions)
+    browser.get(url)
+    delay = 3 # segundos
+    try:
+        myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, 'elementToPDF')))
+        print ("Pagina lista")
+        html = browser.page_source
+        soup = BeautifulSoup(html, 'lxml')
+        div = soup.find("div", {"id": "elementToPDF"})
+        f = open( 'csv/'+url[62:86]+'.csv', 'w' )
+        f.write( str(div) )
+        f.close()
+        browser.close()
+    except TimeoutException:
+        print ("Cargando..")    
